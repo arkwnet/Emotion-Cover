d184d2976b3a18d3c133af4e32291d2bb10c6986
Sora Arakawa <info@arkw.net>
2017-03-25 22:15:15 +0900

    Update 2017/3/25 (Ver.1.0_alpha2)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Emotion-Cover
-Free Slideshow Script for Website
+# Emotion Cover
+ウェブページにスライドショーを設置するオープンソース・スクリプトです。
+HTML5 / CanvasとJavaScriptで構築しています。
+
+## 設置方法
+1. 適当な画像を用意する。(2～3枚)
+
+2. 変数logoを作成し、ロゴ画像を指定する。
+
+3. 配列imagesを作成し、画像、URLの情報を代入する。
+
+## サンプル
+sample.htmlが簡単なサンプルです。是非お使い下さい。
+
+## マニュアル
+準備中
---
+++ js/emotion_cover.js
@@ -0,0 +1,83 @@
+/*
+Emotion Cover Ver.1.0_alpha2
+(c)2015-2017 Sora Arakawa all rights reserved.
+
+This software is MIT License.
+*/
+
+var canvas=document.getElementById("mainCanvas");
+var ctx=canvas.getContext("2d");
+var mode=0;
+var mode2=0;
+var count=0;
+var alpha=0;
+var sid=0;
+var sid_next=1;
+
+window.onload=function(){
+	setInterval(EmotionCoverMain,10);
+}
+
+function EmotionCoverMain(){
+	canvas.addEventListener('click',EmotionCoverOnClick, false);
+	ctx.globalAlpha=1;
+	ctx.fillStyle="rgb(250,250,250)";
+	ctx.fillRect(0,0,960,328);
+	ctx.drawImage(verinfo,6,288);
+	if(mode==0){
+		ctx.globalAlpha=alpha;
+		ctx.drawImage(logo,(canvas.width - 300)/2,(canvas.height - 143)/2,300,143);
+		if(alpha<1){ alpha+=0.01; }
+		if(alpha>0.99){ mode2++; }
+		if(mode2>200){ mode2=0;mode=1; }
+	}
+	if(mode==1){
+		ctx.globalAlpha=alpha;
+		ctx.drawImage(logo,(canvas.width - 300)/2,(canvas.height - 143)/2,300,143);
+		if(alpha>0.01){ alpha-=0.01; }
+		if(alpha<0.02){ alpha=0;mode=2; }
+	}
+	if(mode==2){
+		for(var i=0; i<images.length; i++) {
+			if(mode2==1 || mode2==2){
+				if(sid==i){ ctx.globalAlpha=1; }else{ ctx.globalAlpha=0.2; }
+			}else{
+				ctx.globalAlpha=alpha;
+			}
+			ctx.drawImage(images[i][0],0,i*81,190,81);
+		}
+		ctx.globalAlpha=alpha;
+		ctx.drawImage(images[sid][0],190,0,780,328);
+		if(mode2==1 || mode2==2){
+			ctx.globalAlpha=1-alpha;
+			ctx.drawImage(images[sid_next][0],190,0,780,328);
+		}
+		if(mode2==0){
+			if(alpha<0.99){ alpha+=0.01; }
+			if(alpha>0.98){ alpha=1;mode2=1; }
+		}
+		if(mode2==1){
+			count++;
+			if(count>800){ mode2=2; }
+		}
+		if(mode2==2){
+			alpha-=0.01;
+			if(alpha<0.02){alpha=1;sid++;sid_next++;count=0;mode2=1}
+		}
+		if(sid_next+1>images.length){ sid_next=0; }
+		if(sid+1>images.length){ sid=0; }
+	}
+}
+
+function EmotionCoverOnClick(ec){
+	var rect = ec.target.getBoundingClientRect();
+    ecx = ec.clientX - rect.left;
+    ecy = ec.clientY - rect.top;
+	if(mode!=0 && ecx>200 && ecy>0 && ecx<960 && ecy<320 && images[sid][1]!=""){ location.href=images[sid][1]; }
+	if(mode!=0 && mode2==1 && ecx>10 && ecy>0 && ecx<190 && ecy<228){
+		sid_next=parseInt(ecy/76);
+		alpha=1;
+		mode2=2;
+	}
+	if(mode!=0 && ecx>10 && ecy>282 && ecx<190 && ecy<320){ window.open("http://arkw.net/data/emotion_cover/","_blank"); }
+}
